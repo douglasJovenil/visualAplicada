669f6b2358592b0261d09e0be6ac5aecb8a8ae3c
Douglas dos Santos <douglascastilhos@yahoo.com.br>
2018-10-10 22:02:30 -0300

    Exercício 2 da aula 6 concluido

---
+++ Aula_6_ListasGraficos/Aula_6_ListasGraficos/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_6_ListasGraficos
+{
+    class Aluno
+    {
+        public String Nome { get; set; }
+        public Double Nota { get; set; }
+
+        public Aluno(String nome, Double nota)
+        {
+            Nome = nome;
+            Nota = nota;
+        }
+    }
+}
